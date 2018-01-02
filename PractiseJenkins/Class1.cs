808a99d62834a308f7d1b150cf5f8dbd4c598a54
learnIts <sindhura_medarametla@epam.com>
2018-01-02 17:43:39 +0530

    first commit

---
+++ PractiseJenkins/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseJenkins
+{
+    public class Class1
+    {
+        public static void Test1()
+        {
+            Console.WriteLine("Sindh Demostrating Jenkins");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/SampleJenkinsProj/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleJenkinsProj
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+      
+            public static void Test1()
+        {
+            Console.WriteLine("sample testing by Sindhu");
+            Console.ReadLine();
+        }
+       
+    }
+}
